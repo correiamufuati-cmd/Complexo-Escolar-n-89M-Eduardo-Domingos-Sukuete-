02d99bbfd5fd565ba355c58037ebf1fd7bfedfec
correiamufuati-cmd <correiamufuati@gmail.com>
2026-06-21 17:21:11 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const firebaseConfig = {
+apiKey: "SUA_API_KEY",
+authDomain: "escola-digital-47497.firebaseapp.com",
+projectId: "escola-digital-47497"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
