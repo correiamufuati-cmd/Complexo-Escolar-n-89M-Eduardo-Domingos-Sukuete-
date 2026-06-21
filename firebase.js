e793d8dc95147ee7077a90de48c3840aa1079d49
correiamufuati-cmd <correiamufuati@gmail.com>
2026-06-21 17:34:32 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
 apiKey: "SUA_API_KEY",
