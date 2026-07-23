ed22d9b0dfb709968ec66d4496bd0e79a772f4c9
correiamufuati-cmd <correiamufuati@gmail.com>
2026-07-23 23:23:45 +0100

    Update pdf-reader.js

--- src/services/pdf-reader.js
+++ src/services/pdf-reader.js
@@ -1,1 +1,38 @@
+import * as pdfjsLib from 
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.mjs";
 
+
+// Ler PDF
+export async function lerPDF(file) {
+
+    const arrayBuffer = await file.arrayBuffer();
+
+
+    const pdf = await pdfjsLib.getDocument({
+        data: arrayBuffer
+    }).promise;
+
+
+    let textoCompleto = "";
+
+
+    for(let i = 1; i <= pdf.numPages; i++){
+
+        const pagina = await pdf.getPage(i);
+
+        const conteudo = await pagina.getTextContent();
+
+
+        const textos = conteudo.items.map(
+            item => item.str
+        );
+
+
+        textoCompleto += textos.join(" ") + "\n";
+
+    }
+
+
+    return textoCompleto;
+
+      }
