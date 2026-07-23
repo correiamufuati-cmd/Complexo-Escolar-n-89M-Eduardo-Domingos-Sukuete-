87f0e61382033185adb6f7609e02ab01de374ea0
correiamufuati-cmd <correiamufuati@gmail.com>
2026-07-23 02:53:56 +0100

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -1,1 +1,30 @@
+import { login } from "./auth.js";
 
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (event) => {
+
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+
+  const result = await login(email, password);
+
+
+  if(result.success){
+
+    alert("Login efetuado com sucesso!");
+
+    // Por enquanto entra no dashboard gestor
+    window.location.href = "../src/pages/dashboard-gestor.html";
+
+
+  } else {
+
+    alert("Erro no login: " + result.message);
+
+  }
+
+});
