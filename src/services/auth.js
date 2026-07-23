5ce240935fe69e482e2ddb6367f8a476ef3d4b89
correiamufuati-cmd <correiamufuati@gmail.com>
2026-07-23 02:38:47 +0100

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,1 +1,43 @@
 
+import { auth } from "./firebase.js";
+import { 
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+
+
+// LOGIN
+export async function login(email, password){
+
+  try {
+
+    const result = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    return {
+      success: true,
+      user: result.user
+    };
+
+
+  } catch(error){
+
+    return {
+      success: false,
+      message: error.message
+    };
+
+  }
+
+}
+
+
+// LOGOUT
+export async function logout(){
+
+  await signOut(auth);
+
+}
