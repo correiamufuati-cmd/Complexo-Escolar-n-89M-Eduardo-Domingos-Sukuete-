fa1843ccc6287264ab2ac74a9d03d3e15dab3fa3
correiamufuati-cmd <correiamufuati@gmail.com>
2026-07-25 11:20:20 +0100

    Update pdf-reader.js

--- src/services/pdf-reader.js
+++ src/services/pdf-reader.js
@@ -1,43 +1,8 @@
-// Leitor d0e PDF - primeira fase (teste)
-console.log("PDF READER FOI CARREGADO");
-
-import * as pdfjsLib from 
-"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.mjs";
+console.log("PDF READER OK");
 
 
 export async function lerPDF(file){
 
-    const dados = await file.arrayBuffer();
-
-
-    const pdf = await pdfjsLib.getDocument({
-        data: dados
-    }).promise;
-
-
-    let texto = "";
-
-
-    for(let pagina = 1; pagina <= pdf.numPages; pagina++){
-
-        const page = await pdf.getPage(pagina);
-
-
-        const content = await page.getTextContent();
-
-
-        content.items.forEach(item => {
-
-            texto += item.str + " ";
-
-        });
-
-
-        texto += "\n";
-
-    }
-
-
-    return texto;
+    return "Teste: PDF recebido com sucesso - " + file.name;
 
 }
