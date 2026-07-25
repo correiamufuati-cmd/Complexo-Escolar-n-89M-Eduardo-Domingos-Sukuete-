dbc716f965e2d3c38242021decfcfaaade3cbadc
correiamufuati-cmd <correiamufuati@gmail.com>
2026-07-25 15:06:03 +0100

    Update pdf-reader.js

--- src/services/pdf-reader.js
+++ src/services/pdf-reader.js
@@ -1,43 +1,8 @@
-alert("PDF READER REAL CARREGADO");
+alert("PDF READER CARREGADO");
 
 
 window.lerPDF = async function(file){
 
-    const dados = await file.arrayBuffer();
-
-
-    const pdfjsLib = await import(
-    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.mjs"
-    );
-
-
-    const pdf = await pdfjsLib.getDocument({
-        data: dados
-    }).promise;
-
-
-    let texto = "";
-
-
-    for(let pagina = 1; pagina <= pdf.numPages; pagina++){
-
-        const page = await pdf.getPage(pagina);
-
-        const content = await page.getTextContent();
-
-
-        content.items.forEach(item => {
-
-            texto += item.str + " ";
-
-        });
-
-
-        texto += "\n\n";
-
-    }
-
-
-    return texto;
+    return "Leitor recebeu: " + file.name;
 
 };
