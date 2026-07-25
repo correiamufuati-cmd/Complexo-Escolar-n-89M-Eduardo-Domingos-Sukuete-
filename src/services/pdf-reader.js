3d8a0d125b9fdb2426e2bf05916035530b0407a1
correiamufuati-cmd <correiamufuati@gmail.com>
2026-07-25 11:33:45 +0100

    Update pdf-reader.js

--- src/services/pdf-reader.js
+++ src/services/pdf-reader.js
@@ -1,9 +1,5 @@
 export async function lerPDF(file){
 
-    const texto = 
-    "PDF recebido: " + file.name +
-    "\nTamanho: " + file.size + " bytes";
-
-    return texto;
+    return "PDF recebido com sucesso: " + file.name;
 
 }
