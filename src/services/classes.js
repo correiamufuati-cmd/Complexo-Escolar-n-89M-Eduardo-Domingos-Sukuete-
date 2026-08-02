f11cfb7f98cc53f9e7ec12dd4fb31ec74c3acf95
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-02 01:13:54 +0100

    Update classes.js

--- src/services/classes.js
+++ src/services/classes.js
@@ -1,1 +1,14 @@
-alert("TESTE CLASSES FUNCIONOU");
+import { app } from "./firebase.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+
+alert("classes.js iniciou");
+
+
+const db = getFirestore(app);
+
+
+alert("Firebase ligado");
