542779fe9a74030762dbe80796689cd5a707ca5f
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-02 12:19:23 +0100

    Update studebt-area.js

--- src/services/studebt-area.js
+++ src/services/studebt-area.js
@@ -1,27 +1,34 @@
+alert("student-area.js carregou");
+
+
 const aluno = JSON.parse(
     localStorage.getItem("alunoLogado")
 );
 
 
+alert(JSON.stringify(aluno));
+
+
 if(!aluno){
 
-    alert("Sessão não encontrada");
+    alert("Aluno não encontrado");
 
     window.location.href = "student-login.html";
 
 }
 
 
+
 document.getElementById("nomeAluno").innerHTML =
-"👨‍🎓 " + (aluno.nome || "");
+"👨‍🎓 " + aluno.nome;
 
 
 document.getElementById("codigo").innerHTML =
-"Código: " + (aluno.codigoAluno || "");
+"Código: " + aluno.codigoAluno;
 
 
 document.getElementById("turma").innerHTML =
-"Turma: " + (aluno.turmaNome || "");
+"Turma: " + aluno.turmaNome;
 
 
 document.getElementById("estado").innerHTML =
