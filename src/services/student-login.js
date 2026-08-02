aa32339939dc106a5bc4fb8c58af2453bb4d9414
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-02 11:15:31 +0100

    Create student-login.js

---
+++ src/services/student-login.js
@@ -0,0 +1,121 @@
+import { app } from "../config/firebase.js";
+
+
+import {
+
+getFirestore,
+collection,
+getDocs
+
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+
+const db = getFirestore(app);
+
+
+
+const codigo = document.getElementById("codigoAluno");
+
+const senha = document.getElementById("senhaAluno");
+
+const entrar = document.getElementById("entrarAluno");
+
+
+
+entrar.addEventListener("click", async()=>{
+
+
+const codigoDigitado = codigo.value.trim();
+
+const senhaDigitada = senha.value.trim();
+
+
+
+if(!codigoDigitado || !senhaDigitada){
+
+alert("Preencha os campos");
+
+return;
+
+}
+
+
+
+const turmas = await getDocs(
+collection(db,"turmas")
+);
+
+
+
+let encontrado = false;
+
+
+
+for(const turma of turmas.docs){
+
+
+const alunos = await getDocs(
+
+collection(
+db,
+"turmas",
+turma.id,
+"alunos"
+)
+
+);
+
+
+
+for(const aluno of alunos.docs){
+
+
+const dados = aluno.data();
+
+
+
+if(
+
+dados.codigoAluno === codigoDigitado &&
+
+dados.senhaAcesso === senhaDigitada
+
+){
+
+
+encontrado = true;
+
+
+
+localStorage.setItem(
+"aluno",
+JSON.stringify(dados)
+);
+
+
+
+window.location.href =
+"student-area.html";
+
+
+return;
+
+
+}
+
+
+}
+
+
+}
+
+
+
+if(!encontrado){
+
+alert("Código ou senha incorretos");
+
+}
+
+
+});
