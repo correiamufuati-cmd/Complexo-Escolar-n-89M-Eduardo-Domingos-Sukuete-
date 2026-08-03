50d2acf2a954a0ac84590de0ee9d00fe9c8f823b
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-03 19:40:01 +0100

    Update student-login.js

--- src/services/student-login.js
+++ src/services/student-login.js
@@ -1,95 +1,19 @@
-import { db } from "../config/firebase.js";
-
-import {
-    collection,
-    getDocs,
-    query,
-    where
-} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-
+alert("student-login.js carregou");
 
 const form = document.getElementById("loginAluno");
 
+alert("Form encontrado: " + form);
 
-form.addEventListener("submit", async (e)=>{
+form.addEventListener("submit", (e)=>{
 
     e.preventDefault();
 
+    const codigo = document.getElementById("codigoAluno").value;
+    const senha = document.getElementById("senhaAluno").value;
 
-    const codigo = document
-        .getElementById("codigoAluno")
-        .value
-        .trim();
-
-
-    const senha = document
-        .getElementById("senhaAluno")
-        .value
-        .trim();
-
-
-
-    try {
-
-
-        const alunosRef = collection(db,"alunos");
-
-
-        const q = query(
-            alunosRef,
-            where("codigoAluno","==",codigo),
-            where("senha","==",senha)
-        );
-
-
-        const resultado = await getDocs(q);
-
-
-
-        if(resultado.empty){
-
-            alert("Código ou senha incorretos");
-            return;
-
-        }
-
-
-
-        const doc = resultado.docs[0];
-
-
-        const aluno = {
-
-            id: doc.id,
-            ...doc.data()
-
-        };
-
-
-
-        localStorage.setItem(
-            "alunoLogado",
-            JSON.stringify(aluno)
-        );
-
-
-
-        window.location.href =
-            "student-area.html";
-
-
-
-    }catch(erro){
-
-
-        console.error(erro);
-
-        alert(
-            "Erro ao entrar no sistema"
-        );
-
-
-    }
-
+    alert(
+        "Código: " + codigo +
+        "\nSenha: " + senha
+    );
 
 });
