e98cb1e43f6e38c158036513aca11a4ff3252c96
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-03 19:07:03 +0100

    Update student-login.js

--- src/services/student-login.js
+++ src/services/student-login.js
@@ -1,19 +1,95 @@
-alert("student-login.js carregou");
+import { db } from "../config/firebase.js";
+
+import {
+    collection,
+    getDocs,
+    query,
+    where
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
 
 const form = document.getElementById("loginAluno");
 
-alert("Form encontrado: " + form);
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", async (e)=>{
 
     e.preventDefault();
 
-    const codigo = document.getElementById("codigoAluno").value;
-    const senha = document.getElementById("senhaAluno").value;
 
-    alert(
-        "Código: " + codigo +
-        "\nSenha: " + senha
-    );
+    const codigo = document
+        .getElementById("codigoAluno")
+        .value
+        .trim();
+
+
+    const senha = document
+        .getElementById("senhaAluno")
+        .value
+        .trim();
+
+
+
+    try {
+
+
+        const alunosRef = collection(db,"alunos");
+
+
+        const q = query(
+            alunosRef,
+            where("codigoAluno","==",codigo),
+            where("senha","==",senha)
+        );
+
+
+        const resultado = await getDocs(q);
+
+
+
+        if(resultado.empty){
+
+            alert("Código ou senha incorretos");
+            return;
+
+        }
+
+
+
+        const doc = resultado.docs[0];
+
+
+        const aluno = {
+
+            id: doc.id,
+            ...doc.data()
+
+        };
+
+
+
+        localStorage.setItem(
+            "alunoLogado",
+            JSON.stringify(aluno)
+        );
+
+
+
+        window.location.href =
+            "student-area.html";
+
+
+
+    }catch(erro){
+
+
+        console.error(erro);
+
+        alert(
+            "Erro ao entrar no sistema"
+        );
+
+
+    }
+
 
 });
