ee113e7cb71052bd5c519d3ab440e904e75a3584
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-03 18:07:36 +0100

    Update student-login.js

--- src/services/student-login.js
+++ src/services/student-login.js
@@ -1,1 +1,19 @@
 alert("student-login.js carregou");
+
+const form = document.getElementById("loginAluno");
+
+alert("Form encontrado: " + form);
+
+form.addEventListener("submit", (e)=>{
+
+    e.preventDefault();
+
+    const codigo = document.getElementById("codigoAluno").value;
+    const senha = document.getElementById("senhaAluno").value;
+
+    alert(
+        "Código: " + codigo +
+        "\nSenha: " + senha
+    );
+
+});
