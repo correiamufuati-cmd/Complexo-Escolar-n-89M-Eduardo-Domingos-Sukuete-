c6cf31cd59311b8e1a475a02431285ccd82dec59
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-03 04:27:04 +0100

    Update student-grades.js

--- src/services/student-grades.js
+++ src/services/student-grades.js
@@ -1,25 +1,147 @@
-const aluno = JSON.parse(
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+
+const tabela = document.getElementById("listaNotas");
+
+
+const alunoLogado = JSON.parse(
     localStorage.getItem("alunoLogado")
 );
 
 
-if(!aluno){
 
-alert("Sessão não encontrada");
+if(!alunoLogado){
 
-window.location.href="student-area.html";
+    alert("Aluno não encontrado");
 
-}
-else{
-
-
-document.getElementById("nomeAluno").innerHTML =
-"👨‍🎓 " + aluno.nome;
-
-
-document.getElementById("turma").innerHTML =
-"Turma: " + aluno.turmaNome;
-
-
+    window.location.href =
+    "../login-aluno.html";
 
 }
+
+
+
+async function carregarNotas(){
+
+
+try{
+
+
+    const turmasSnapshot =
+    await getDocs(collection(db,"turmas"));
+
+
+
+    for(const turmaDoc of turmasSnapshot.docs){
+
+
+        const alunosSnapshot =
+        await getDocs(
+            collection(
+                db,
+                "turmas",
+                turmaDoc.id,
+                "alunos"
+            )
+        );
+
+
+
+        for(const alunoDoc of alunosSnapshot.docs){
+
+
+            if(alunoDoc.id === alunoLogado.id){
+
+
+
+                const notasSnapshot =
+                await getDocs(
+                    collection(
+                        db,
+                        "turmas",
+                        turmaDoc.id,
+                        "alunos",
+                        alunoDoc.id,
+                        "notas"
+                    )
+                );
+
+
+
+                notasSnapshot.forEach((notaDoc)=>{
+
+
+                    const nota =
+                    notaDoc.data();
+
+
+
+                    tabela.innerHTML += `
+
+                    <tr>
+
+                    <td>
+                    ${notaDoc.id}
+                    </td>
+
+                    <td>
+                    ${nota.MAC || ""}
+                    </td>
+
+                    <td>
+                    ${nota.NPT || ""}
+                    </td>
+
+                    <td>
+                    ${nota.MF || ""}
+                    </td>
+
+                    </tr>
+
+                    `;
+
+
+                });
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+
+}catch(error){
+
+
+console.error(error);
+
+alert(
+"Erro ao carregar notas"
+);
+
+
+}
+
+
+}
+
+
+
+carregarNotas();
+
+
+
+window.voltar=function(){
+
+history.back();
+
+};
