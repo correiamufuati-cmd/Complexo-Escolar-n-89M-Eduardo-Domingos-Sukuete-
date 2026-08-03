6aef5177eee59780a9e126dd7c053940dedc878e
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-03 07:58:10 +0100

    Update and rename login.js to gestor-login.js

---
+++ src/services/gestor-login.js
@@ -0,0 +1,46 @@
+import { login } from "./auth.js";
+
+const form = document.getElementById("loginForm");
+
+
+form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+
+    const email =
+    document.getElementById("email").value;
+
+
+    const password =
+    document.getElementById("password").value;
+
+
+
+    const result =
+    await login(email, password);
+
+
+
+    if(result.success){
+
+
+        alert("Login efetuado com sucesso!");
+
+
+        window.location.href =
+        "../pages/dashboard-gestor.html";
+
+
+    }else{
+
+
+        alert(
+        "Erro no login: " + result.message
+        );
+
+
+    }
+
+
+});
