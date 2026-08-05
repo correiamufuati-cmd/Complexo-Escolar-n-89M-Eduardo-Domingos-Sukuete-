513f9d86e4818530fdb64178631330495c8ff385
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-05 04:00:57 +0100

    Update pdf-reader.js

--- src/services/pdf-reader.js
+++ src/services/pdf-reader.js
@@ -1,58 +1,14 @@
-import * as pdfjsLib from 
-"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.mjs";
-
-
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.mjs";
+alert("PDF-READER FOI CARREGADO");
 
 
 export async function lerPDF(file){
 
-    alert("ENTROU NO LEITOR PDF");
-
-
-    const dados = await file.arrayBuffer();
-
-
-    const pdf = await pdfjsLib.getDocument({
-        data:dados
-    }).promise;
-
-
-    alert("PDF ABERTO");
-
-
-    let textoCompleto = "";
-
-
-    for(let pagina = 1; pagina <= pdf.numPages; pagina++){
-
-        const page = await pdf.getPage(pagina);
-
-        const conteudo = await page.getTextContent();
-
-
-        conteudo.items.forEach(item=>{
-
-            textoCompleto += item.str + " ";
-
-        });
-
-    }
-
-
-    alert("Texto extraído");
-
+    alert("FUNÇÃO LER PDF");
 
     return {
-
         quantidade:0,
-
         alunos:[],
-
-        texto:textoCompleto
-
+        texto:""
     };
-
 
 }
