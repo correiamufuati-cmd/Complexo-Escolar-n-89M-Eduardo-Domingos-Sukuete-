7b0b0464697f112649f655c127789aac86b372ce
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-05 05:16:24 +0100

    Update pdf-reader.js

--- src/services/pdf-reader.js
+++ src/services/pdf-reader.js
@@ -1,14 +1,72 @@
-alert("PDF-READER FOI CARREGADO");
+alert("PDF-READER FOI CARREGADO Df");
+
+
+import * as pdfjsLib from 
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.mjs";
+
+
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs";
+
 
 
 export async function lerPDF(file){
 
-    alert("FUNÇÃO LER PDF");
+
+    alert("A abrir PDF");
+
+
+    const arrayBuffer = await file.arrayBuffer();
+
+
+    const pdf = await pdfjsLib.getDocument({
+        data: arrayBuffer
+    }).promise;
+
+
+
+    let textoCompleto = "";
+
+
+
+    for(let i=1;i<=pdf.numPages;i++){
+
+
+        const pagina = await pdf.getPage(i);
+
+
+        const conteudo = await pagina.getTextContent();
+
+
+
+        const textoPagina = conteudo.items
+        .map(item=>item.str)
+        .join(" ");
+
+
+
+        textoCompleto += textoPagina + "\n";
+
+
+    }
+
+
+
+    alert(
+        "Texto extraído: " + textoCompleto.length + " caracteres"
+    );
+
+
 
     return {
+
         quantidade:0,
+
         alunos:[],
-        texto:""
+
+        texto:textoCompleto
+
     };
 
-}
+
+        }
