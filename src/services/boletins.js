79e9683ee907f407e80749d0acd5f196c1988dd1
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-10 14:57:47 +0100

    Update boletins.js

--- src/services/boletins.js
+++ src/services/boletins.js
@@ -1,9 +1,111 @@
-// ============================================
-// TESTE 1 — CARREGAMENTO DO BOLETINS.JS
-// ============================================
+// =====================================================
+// BOLETINS.JS
+// ETAPA 3 — TESTE DOS FILTROS
+// =====================================================
 
-alert("✅ BOLETINS.JS CARREGOU!");
+alert("✅ BOLETINS.JS 1 CARREGOU!");
 
-console.log(
-    "BOLETINS.JS CARREGADO CORRETAMENTE"
+
+const classeSelect =
+    document.getElementById("classeSelect");
+
+
+const turmaSelect =
+    document.getElementById("turmaSelect");
+
+
+const trimestreSelect =
+    document.getElementById("trimestreSelect");
+
+
+const pesquisaAluno =
+    document.getElementById("pesquisaAluno");
+
+
+if(
+    !classeSelect ||
+    !turmaSelect ||
+    !trimestreSelect ||
+    !pesquisaAluno
+){
+
+    alert(
+        "❌ ERRO: algum campo dos filtros não foi encontrado."
+    );
+
+}
+else{
+
+    alert(
+        "✅ OS 4 CAMPOS DOS FILTROS FORAM ENCONTRADOS!"
+    );
+
+}
+
+
+// =====================================================
+// TESTAR CLASSE
+// =====================================================
+
+classeSelect?.addEventListener(
+    "change",
+    function(){
+
+        alert(
+            "Classe selecionada: " +
+            this.value
+        );
+
+    }
+);
+
+
+// =====================================================
+// TESTAR TURMA
+// =====================================================
+
+turmaSelect?.addEventListener(
+    "change",
+    function(){
+
+        alert(
+            "Turma selecionada: " +
+            this.value
+        );
+
+    }
+);
+
+
+// =====================================================
+// TESTAR TRIMESTRE
+// =====================================================
+
+trimestreSelect?.addEventListener(
+    "change",
+    function(){
+
+        alert(
+            "Trimestre selecionado: " +
+            this.value
+        );
+
+    }
+);
+
+
+// =====================================================
+// TESTAR PESQUISA
+// =====================================================
+
+pesquisaAluno?.addEventListener(
+    "input",
+    function(){
+
+        console.log(
+            "Pesquisa:",
+            this.value
+        );
+
+    }
 );
