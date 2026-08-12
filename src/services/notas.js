5fb9773b3c4a1a0ff24cf88c8683da5e4d702e6a
correiamufuati-cmd <correiamufuati@gmail.com>
2026-08-12 05:34:34 +0100

    Update notas.js

--- src/services/notas.js
+++ src/services/notas.js
@@ -1,8 +1,3 @@
-// =====================================================
-// NOTAS.JS — ADMINISTRADOR
-// BLOCO 1 — TESTE
-// =====================================================
-
 alert(
     "🔥 NOTAS.JS DO ADMINISTRADOR ESTÁ A FUNCIONAR!"
 );
